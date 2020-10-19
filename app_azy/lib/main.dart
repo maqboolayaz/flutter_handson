8e72ff7427378eed907aaf51eb3ef53210d963e1
Ayaz Maqbool <ayazmaqbooluet@gmail.com>
2020-10-20 01:10:47 +0200

    refactoring of code done

--- app_azy/lib/main.dart
+++ app_azy/lib/main.dart
@@ -1,96 +1,20 @@
 import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
 
 
 void main(){
-  runApp(MaterialApp(
-    home: HomePage(),
-    theme: ThemeData(
-      primarySwatch: Colors.purple
-     ),
-  ));
+  runApp(MyApp());
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  var myText = "Change Item Name";
-  TextEditingController _nameController = TextEditingController();
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text("New App")),
-      body: Center (
-        child: SingleChildScrollView(
-                  child: Card(
-            child: Column(
-              children: <Widget>[
-                Image.asset(
-                  "assets/laptop.jpeg"
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  myText,
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)
-                ),
-                Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: TextField(
-                      controller: _nameController,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "Enter text here",
-                        labelText: "My label",
-                      ),
-                    ),
-                  )
-              ],
-              ),
-          ),
-        )
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.all(0),
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: Text("Ayaz Maq"),
-              accountEmail: Text("itsayazmaqbool@gmail.com"),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(
-                  "https://images.unsplash.com/photo-1530695440407-21fef47230b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80") 
-              )
-            ),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text("Ayaz Maq"),
-              subtitle: Text("Developer"),
-              trailing: Icon(Icons.edit),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text("Email"),
-              subtitle: Text("itsayazmaqbool@gmail.com"),
-              trailing: Icon(Icons.edit),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          myText = _nameController.text;
-          setState(() {});
-        },
-        child: Icon(Icons.edit),
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.purple
       ),
     );
   }
 }
+
